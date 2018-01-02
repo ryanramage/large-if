94ee5433d6a2bade68ece6552d2514c710e07a87
Ryan Ramage <ryan.ramage@gmail.com>
2018-01-02 15:59:22 -0700

    seems to work

--- test/unit.js
+++ test/unit.js
@@ -3,24 +3,24 @@ const fs = require('fs')
 const path = require('path')
 const test = require('tape')
 const csv = require('csv-parser')
-const columns = ['a', 'b', 'c']
-const decisionField = 'result'
+const csv2 = require('csv2')
+const JSONStream = require('JSONStream')
+const columns = ['a', 'b', 'c', 'result']
 
 const csvLocation = path.resolve(__dirname, 'table.csv')
 
 test('basic test', function (t) {
-  let opts = {memoryDb: '/a'}
-  let table = new DecisionTable(columns, decisionField, opts, err => {
+  let table = new DecisionTable(columns)
+  let ndjsonStream = fs.createReadStream(csvLocation)
+    .pipe(csv2())
+  table.load(ndjsonStream, err => {
     t.error(err)
-    let ndjsonStream = fs.createReadStream(csvLocation).pipe(csv())
-    table.load(ndjsonStream, err => {
+    table.decide(['green', 'little', 'lots'], (err, category) => {
       t.error(err)
-      table.decide(['happy', 'little', 'lots'], (err, category) => {
-        t.error(err)
-        t.equals(category, 'ok')
-        t.ok(true)
-        t.end()
-      })
+      console.log(category)
+      t.equals(category, 'ok')
+      t.ok(true)
+      t.end()
     })
   })
 })
